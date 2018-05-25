5001fe07c3fa0b1d55235398ba3f00e4269ed7ea
hzliyongan <hzliyonga@corp.netease.com>
2018-05-25 10:59:54 +0800

    使用gradle脚本，在MainActivity销毁时上传jacoco数据

--- app/src/main/java/com/lya/testjacoco/MainActivity.java
+++ app/src/main/java/com/lya/testjacoco/MainActivity.java
@@ -1,8 +1,8 @@
 package com.lya.testjacoco;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -10,6 +10,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.text.SimpleDateFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,5 +30,35 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        if (BuildConfig.DEBUG) {
+            generateCoverReport();
+        }
+    }
+
+    private void generateCoverReport() {
+        OutputStream out = null;
+        try {
+            File coverageDir = new File(Environment.getExternalStorageDirectory() + File.separator + "coverage"); //这里是写入覆盖率工具的 sd 卡上的目录
+            if(!coverageDir.exists()){
+                coverageDir.mkdirs();
+            }
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmssSSS");
+            String coverageFileName = BuildConfig.VERSION_NAME + "_" + sdf.format(new java.util.Date())+".ec";
+            File file = new File(coverageDir,coverageFileName);
+            out = new FileOutputStream(file, false);
+            Object agent = Class.forName("org.jacoco.agent.rt.RT").getMethod("getAgent").invoke(null);
+            out.write((byte[])agent.getClass().getMethod("getExecutionData", boolean.class).invoke(agent, true));
+            //第二个bool 参数是指，获得数据后，是否reset data，重新生成覆盖数据
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
